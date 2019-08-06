91db50665db34c5bb893a1fd3dbcde1ad943889e
renatas7 <renatas.net@gmail.com>
2019-08-06 10:53:34 +0300

    Updates

---
+++ precache-manifest.4c15080f7d7bc69b77dd319982757d88.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0c936217060dbb8950c5522d6ec69df2",
+    "url": "/user-register-form2/index.html"
+  },
+  {
+    "revision": "87e43cc131cff1a730bf",
+    "url": "/user-register-form2/static/css/main.eb94555f.chunk.css"
+  },
+  {
+    "revision": "e8bcc5171d2f8a7373e3",
+    "url": "/user-register-form2/static/js/2.d9643184.chunk.js"
+  },
+  {
+    "revision": "87e43cc131cff1a730bf",
+    "url": "/user-register-form2/static/js/main.59369803.chunk.js"
+  },
+  {
+    "revision": "fe4fa95133eac4aad0db",
+    "url": "/user-register-form2/static/js/runtime~main.b1aa7cf2.js"
+  }
+]);
